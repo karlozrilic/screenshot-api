50123d58b1b9c516c799cc03918d3f9cf431db1e
karlozrilic <karlozrilic@gmail.com>
2026-06-07 21:39:08 +0200

    Update

--- public/js/capture.js
+++ public/js/capture.js
@@ -43,7 +43,7 @@ export async function capture() {
                 <div class="card-footer">
                     <span class="card-url" title="${escapeHtml(url)}">${escapeHtml(displayUrl)}</span>
                     <div class="card-actions">
-                        <a class="action-btn" href="${escapeHtml(data.image)}" download="screenshot.png">
+                        <a class="action-btn" href="${escapeHtml(data.image)}" download="${displayUrl}-screenshot.png">
                             <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M5.5 1v6M2.5 5l3 3 3-3M1 10h9" stroke="currentColor" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round"/>
                             </svg>
